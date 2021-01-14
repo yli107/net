7011e9700307edf286bf936ad3d9370741c10ebc
yli107 <yli107@ncsu.edu>
2021-01-14 13:55:09 +0800

    updated ServerIO and test cases

--- Net/src/main/java/edu/study/net/tcp/TcpServerIO.java
+++ Net/src/main/java/edu/study/net/tcp/TcpServerIO.java
@@ -7,8 +7,32 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class TcpServerIO {
+
+	private static final int DEFAULT_PORT = 2077;
+
+	private int port;
+
+	public TcpServerIO(int port) {
+		setPort(port);
+	}
+
+	public TcpServerIO() {
+		setPort(DEFAULT_PORT);
+	}
+
+	public void setPort(int port) {
+		if (port > 65535 || port < 0) {
+			throw new IndexOutOfBoundsException("The port must be in range of 0 - 65535");
+		}
+		this.port = port;
+	}
+
+	public int getPort() {
+		return port;
+	}
+
 	public String accept() throws IOException {
-		ServerSocket serverSocket = new ServerSocket(2077);
+		ServerSocket serverSocket = new ServerSocket(port);
 		Socket socket = serverSocket.accept();
 		InputStream is = socket.getInputStream();
 		ByteArrayOutputStream baos = new ByteArrayOutputStream();
--- Net/src/test/java/edu/study/net/tcp/TestTcpServer01.java
+++ Net/src/test/java/edu/study/net/tcp/TestTcpServer01.java
@@ -7,7 +7,7 @@ import java.io.IOException;
 import org.junit.Before;
 import org.junit.Test;
 
-public class TestServer01 {
+public class TestTcpServer01 {
 	private TcpServerIO serverIO;
 
 	@Before
